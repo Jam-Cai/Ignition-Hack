f11ddd25a31588d921f1624f8033733208f6d055
n-arms <75282394+n-arms@users.noreply.github.com>
2022-08-27 22:28:49 -0400

    Add dial to display dBA

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ import DecibelMeter from 'decibel-meter'
 
 const decibelOutputNode = document.getElementById("decibel-output")
 
+const dialNode = document.getElementById("dial")
+
+const renderDial = (canvasNode, min, max, value) => {
+    const ctx = canvasNode.getContext("2d")
+    const width = canvasNode.width
+    const height = canvasNode.height
+
+    const radiansFromZero = Math.PI * (value - min) / (max - min)
+
+    ctx.clearRect(0, 0, width, height)
+    ctx.beginPath()
+    ctx.arc(width / 2, height / 2, width / 2, Math.PI, 0)
+    ctx.stroke()
+    ctx.translate(width / 2, height / 2)
+    ctx.rotate(radiansFromZero)
+    ctx.beginPath()
+    ctx.moveTo(0, 0)
+    ctx.lineTo(-width / 2, 0)
+    ctx.stroke()
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
+}
+
 let locked = false
 
 const meter = new DecibelMeter
@@ -9,6 +31,7 @@ meter.sources.then(console.log)
 meter.listenTo(0, db => {
     if (!locked) {
         decibelOutputNode.innerHTML = db + 130
+        renderDial(dialNode, 0, 140, db + 130)
         locked = true
         setTimeout(_ => locked = false, 100)
     }
@@ -38,66 +61,4 @@ const renderGraph = (canvasNode, startX, endX, startY, endY, data) => {
         ctx.stroke()
         last = [x, y]
     })
-}
-
-renderGraph(graphNode, 0, 1, 0, 1, [[0, 0], [0.5, 0.25], [1, 1]])
-
-const soundInputNode = document.getElementById("sound-input")
-const recordButtonNode = document.getElementById("record")
-
-
-// Set up the AudioContext.
-const audioCtx = new AudioContext();
-
-// Top-level variable keeps track of whether we are recording or not.
-let recording = false;
-
-// Ask user for access to the microphone.
-if (navigator.mediaDevices) {
-    navigator.mediaDevices.getUserMedia({"audio": true}).then((stream) => {
-
-        // Instantiate the media recorder.
-        const mediaRecorder = new MediaRecorder(stream);
-
-        // Create a buffer to store the incoming data.
-        let chunks = [];
-        mediaRecorder.ondataavailable = (event) => {
-          chunks.push(event.data);
-        }
-
-        // When you stop the recorder, create a empty audio clip.
-        mediaRecorder.onstop = (event) => {
-            const audio = new Audio();
-            audio.setAttribute("controls", "");
-            soundInputNode.appendChild(audio)
-            soundInputNode.appendChild(document.createElement("br"));
-
-            // Combine the audio chunks into a blob, then point the empty audio clip to that blob.
-            const blob = new Blob(chunks, {"type": "audio/ogg; codecs=opus"});
-            audio.src = window.URL.createObjectURL(blob);
-
-            // Clear the `chunks` buffer so that you can record again.
-            chunks = [];
-        };
-
-        // Set up event handler for the "Record" button.
-        recordButtonNode.addEventListener("click", () => {
-            if (recording) {
-                mediaRecorder.stop();
-                recording = false;
-                recordButtonNode.innerHTML = "Record";
-            } else {
-                mediaRecorder.start();
-                recording = true;
-                recordButtonNode.innerHTML = "Stop";
-            }
-        });
-
-    }).catch((err) => {
-        // Throw alert when the browser is unable to access the microphone.
-        alert("Your browser cannot access your computer's microphone. Try reconnecting or troubleshooting your microphone");
-    });
-} else {
-    // Throw alert when the browser cannot access any media devices.
-    alert("Oh no! Your browser cannot access your computer's microphone. Please update your browser.");
 }
