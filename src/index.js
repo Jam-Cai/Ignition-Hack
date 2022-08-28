52df64bfd58a5b6a75acc0df1599a9fb09614ba7
n-arms <75282394+n-arms@users.noreply.github.com>
2022-08-27 21:06:21 -0400

    Now with 100% less jquery

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import $ from 'jquery'
-
-
 const graphNode = document.getElementById("graph")
 
 const renderGraph = (canvasNode, startX, endX, startY, endY, data) => {
@@ -26,6 +23,9 @@ const renderGraph = (canvasNode, startX, endX, startY, endY, data) => {
 
 renderGraph(graphNode, 0, 1, 0, 1, [[0, 0], [0.5, 0.25], [1, 1]])
 
+const soundInputNode = document.getElementById("sound-input")
+const recordButtonNode = document.getElementById("record")
+
 
 // Set up the AudioContext.
 const audioCtx = new AudioContext();
@@ -35,50 +35,50 @@ let recording = false;
 
 // Ask user for access to the microphone.
 if (navigator.mediaDevices) {
-  navigator.mediaDevices.getUserMedia({"audio": true}).then((stream) => {
+    navigator.mediaDevices.getUserMedia({"audio": true}).then((stream) => {
 
-    // Instantiate the media recorder.
-    const mediaRecorder = new MediaRecorder(stream);
+        // Instantiate the media recorder.
+        const mediaRecorder = new MediaRecorder(stream);
 
-    // Create a buffer to store the incoming data.
-    let chunks = [];
-    mediaRecorder.ondataavailable = (event) => {
-      chunks.push(event.data);
-    }
+        // Create a buffer to store the incoming data.
+        let chunks = [];
+        mediaRecorder.ondataavailable = (event) => {
+          chunks.push(event.data);
+        }
 
-    // When you stop the recorder, create a empty audio clip.
-    mediaRecorder.onstop = (event) => {
-      const audio = new Audio();
-      audio.setAttribute("controls", "");
-      $("#sound-input").append(audio);
-      $("#sound-input").append("<br />");
+        // When you stop the recorder, create a empty audio clip.
+        mediaRecorder.onstop = (event) => {
+            const audio = new Audio();
+            audio.setAttribute("controls", "");
+            soundInputNode.appendChild(audio)
+            soundInputNode.appendChild(document.createElement("br"));
 
-      // Combine the audio chunks into a blob, then point the empty audio clip to that blob.
-      const blob = new Blob(chunks, {"type": "audio/ogg; codecs=opus"});
-      audio.src = window.URL.createObjectURL(blob);
+            // Combine the audio chunks into a blob, then point the empty audio clip to that blob.
+            const blob = new Blob(chunks, {"type": "audio/ogg; codecs=opus"});
+            audio.src = window.URL.createObjectURL(blob);
 
-      // Clear the `chunks` buffer so that you can record again.
-      chunks = [];
-    };
+            // Clear the `chunks` buffer so that you can record again.
+            chunks = [];
+        };
 
-    // Set up event handler for the "Record" button.
-    $("#record").on("click", () => {
-      if (recording) {
-        mediaRecorder.stop();
-        recording = false;
-        $("#record").html("Record");
-      } else {
-        mediaRecorder.start();
-        recording = true;
-        $("#record").html("Stop");
-      }
+        // Set up event handler for the "Record" button.
+        recordButtonNode.addEventListener("click", () => {
+            if (recording) {
+                mediaRecorder.stop();
+                recording = false;
+                recordButtonNode.innerHTML = "Record";
+            } else {
+                mediaRecorder.start();
+                recording = true;
+                recordButtonNode.innerHTML = "Stop";
+            }
+        });
+
+    }).catch((err) => {
+        // Throw alert when the browser is unable to access the microphone.
+        alert("Your browser cannot access your computer's microphone. Try reconnecting or troubleshooting your microphone");
     });
-
-  }).catch((err) => {
-    // Throw alert when the browser is unable to access the microphone.
-    alert("Your browser cannot access your computer's microphone. Try reconnecting or troubleshooting your microphone");
-  });
 } else {
-  // Throw alert when the browser cannot access any media devices.
-  alert("Oh no! Your browser cannot access your computer's microphone. Please update your browser.");
+    // Throw alert when the browser cannot access any media devices.
+    alert("Oh no! Your browser cannot access your computer's microphone. Please update your browser.");
 }
